8eeee3455a1186de77daf0b23e68ef36f6e30206
Dave Gomez <davegomez426@gmail.com>
2023-08-27 20:45:06 -0400

    session de redis

---
+++ src/session/Session.ts
@@ -0,0 +1,25 @@
+import { SessionOptions } from 'express-session';
+import { getRedisStore } from '../Redis/RedisStore';
+
+export const getSessionData = async () => {
+    try {
+        const store = await getRedisStore();
+        const sessionData: SessionOptions = {
+            store: undefined,
+            secret: process.env.CLIENT_SECRET || 'public',
+            resave: false,
+            saveUninitialized: true,
+            name: 'session',
+            cookie: {
+                secure: false,
+                httpOnly: true,
+            },
+        };
+
+        if (store) {
+            sessionData.store = store;
+        }
+
+        return sessionData;
+    } catch (error) {}
+};
